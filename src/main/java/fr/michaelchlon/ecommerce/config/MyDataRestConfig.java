db793486ebef9efbdd7cc888a0458aaafa7ea156
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-10-17 14:51:14 +0200

    feat: update CORS and HTTP method restrictions
    - Add CrossOrigin annotation to Product and ProductCategory repositories
    - Block PATCH method for Product and ProductCategory endpoints
    - Clean up formatting and comments in MyDataRestConfig class

--- src/main/java/fr/michaelchlon/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/fr/michaelchlon/ecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,6 @@ package fr.michaelchlon.ecommerce.config;
 
 import fr.michaelchlon.ecommerce.entity.Product;
 import fr.michaelchlon.ecommerce.entity.ProductCategory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.metamodel.EntityType;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -14,57 +9,63 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
-  private EntityManager entityManager;
 
-  @Autowired
-  public MyDataRestConfig(EntityManager theEntityManager) {
-    this.entityManager = theEntityManager;
-  }
+    private EntityManager entityManager;
 
-  @Override
-  public void configureRepositoryRestConfiguration(
-      RepositoryRestConfiguration config, CorsRegistry cors) {
-    RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
-    HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
-
-    // Disable HTTP methods for Product: PUT, POST, DELETE
-    config
-        .getExposureConfiguration()
-        .forDomainType(Product.class)
-        .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-        .withCollectionExposure(
-            (metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-    // Disable HTTP methods for ProductCategory: PUT, POST, DELETE
-    config
-        .getExposureConfiguration()
-        .forDomainType(ProductCategory.class)
-        .withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-        .withCollectionExposure(
-            (metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-
-    // call an internal helper method
-    exposeIds(config);
-  }
-
-  private void exposeIds(RepositoryRestConfiguration config) {
-    // expose entity ids
-
-    // - get a list of all entity classes from the entity manager
-    Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
-
-    // - create an arrray of the entity types
-    List<Class> entityClasses = new ArrayList<>();
-
-    // - get the entity types for the entities
-    for (var tempEntityType : entities) {
-      entityClasses.add(tempEntityType.getJavaType());
+    @Autowired
+    public MyDataRestConfig(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
     }
 
-    // - expose the entity ids for the array of entity/domain types
-    Class[] domainTypes = entityClasses.toArray(new Class[0]);
-    config.exposeIdsFor(domainTypes);
-  }
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+            .forDomainType(Product.class)
+            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+        config.getExposureConfiguration()
+            .forDomainType(ProductCategory.class)
+            .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+            .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // call an internal helper method
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // expose entity ids
+        //
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+
 }
