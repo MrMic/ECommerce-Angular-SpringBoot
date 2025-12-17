6819105fdf65b00b42ed1ff368d7ba5a606da7eb
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-12-17 15:57:01 +0100

    feat(address): Add Country and State entities for shipping - 166/
    Introduce Country and State JPA entities to support geographical
    address information. This lays the groundwork for shipping and
    billing functionality.
    
    Also includes an `.nvmrc` file to specify the Node.js version
    for frontend development, and minor formatting adjustments
    to `pom.xml`.

---
+++ src/main/java/fr/michaelchlon/ecommerce/entity/Country.java
@@ -0,0 +1,31 @@
+package fr.michaelchlon.ecommerce.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "country")
+@Getter
+@Setter
+public class Country {
+  @jakarta.persistence.Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "code")
+  private String code;
+
+  @Column(name = "name")
+  private String name;
+
+  @OneToMany(mappedBy = "country")
+  private List<State> states;
+}
