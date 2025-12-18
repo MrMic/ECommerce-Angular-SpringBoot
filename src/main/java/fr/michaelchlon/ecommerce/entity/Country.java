04fec9ba595e010fb6c4b41ece0e492151541bdd
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-12-18 15:21:02 +0100

    feat(api): expose country and state resources - 167/
    - This commit introduces new API endpoints for countries and states.
    -Adds `CountryRepository` and `StateRepository` to expose these entities
    through Spring Data REST. The `/states/search/findByCountryCode`
    endpoint is available to find all states for a given country.
    - Updates `Country` and `State` entities with the appropriate
    one-to-many and many-to-one relationships.
    - Includes `spring-boot-starter-data-rest` dependency in `pom.xml`.
    Configures the database connection and JPA properties in
    `application.properties`.
    - A `requests.http` file is also added to provide sample requests for
    the new endpoints.

--- src/main/java/fr/michaelchlon/ecommerce/entity/Country.java
+++ src/main/java/fr/michaelchlon/ecommerce/entity/Country.java
@@ -1,5 +1,6 @@
 package fr.michaelchlon.ecommerce.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,5 +28,6 @@ public class Country {
   private String name;
 
   @OneToMany(mappedBy = "country")
+  @JsonIgnore
   private List<State> states;
 }
